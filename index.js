1c285f285a4296dfddd586744888e0115bf690a1
Alexander Ryden <alexander.ryden@gmail.com>
2018-12-15 10:27:01 +0100

    Discover devices and create accessories

--- index.js
+++ index.js
@@ -1,42 +1,84 @@
+const shellies = require('shellies')
 
 module.exports = homebridge => {
-  const Service = homebridge.hap.Service
+  const Accessory = homebridge.hap.Accessory
   const Characteristic = homebridge.hap.Characteristic
-
-  class ShellyAccessory {
-    constructor(log) {
-      this.log = log
-      this.id = '123456'
-      this.name = 'Shelly Accessory'
-    }
-
-    identify() {
-      this.log('Shelly device identified')
-    }
-
-    getServices() {
-      const informationService = new Service.AccessoryInformation()
-        .setCharacteristic(Characteristic.Manufacturer, 'Shelly')
-        .setCharacteristic(Characteristic.Model, '1')
-        .setCharacteristic(Characteristic.SerialNumber, '123456')
-
-      const switchService = new Service.Switch()
-        .setCharacteristic(Characteristic.On, true)
-
-      return [informationService, switchService]
-    }
-  }
+  const PlatformAccessory = homebridge.platformAccessory
+  const Service = homebridge.hap.Service
+  const UUIDGen = homebridge.hap.uuid
 
   class ShellyPlatform {
-    constructor(log, config) {
+    constructor(log, config, api) {
       this.log = log
       this.config = config
+      this.api = api
+
+      shellies.on('discover', this.discoverDeviceHandler.bind(this))
+      this.api.on('didFinishLaunching', () => { shellies.start() })
     }
 
-    accessories(callback) {
-      callback([new ShellyAccessory(this.log)])
+    discoverDeviceHandler(device) {
+      const accessory = this.createAccessory(device)
+      this.api.registerPlatformAccessories(
+        'homebridge-shelly',
+        'Shelly',
+        [accessory]
+      )
+    }
+
+    createAccessory(device) {
+      const name = device.name || `${device.type} ${device.id}`
+      const accessory = new PlatformAccessory(
+        name,
+        UUIDGen.generate(name),
+        Accessory.Categories.SWITCH
+      )
+
+      accessory.getService(Service.AccessoryInformation)
+        .setCharacteristic(Characteristic.Manufacturer, 'Shelly')
+        .setCharacteristic(Characteristic.Model, device.type)
+        .setCharacteristic(Characteristic.SerialNumber, device.id)
+        .setCharacteristic(Characteristic.FirmwareRevision, device.settings.fw)
+
+      accessory.addService(
+        new Service.Switch()
+          .setCharacteristic(Characteristic.On, true)
+      )
+
+      accessory.context = {
+        type: device.type,
+        id: device.id,
+        host: device.host,
+      }
+
+      return accessory
+    }
+
+    async configureAccessory(accessory) {
+      const ctx = accessory.context
+      const device = shellies.createDevice(ctx.type, ctx.id, ctx.host)
+
+      shellies.addDevice(device)
+
+      try {
+        device.settings = await device.getSettings()
+      } catch (e) {
+        this.log(
+          'Device with ID',
+          device.id,
+          'and address',
+          device.host,
+          'unreachable'
+        )
+        device.online = false
+      }
     }
   }
 
-  homebridge.registerPlatform('homebridge-shelly', 'Shelly', ShellyPlatform)
+  homebridge.registerPlatform(
+    'homebridge-shelly',
+    'Shelly',
+    ShellyPlatform,
+    true
+  )
 }
