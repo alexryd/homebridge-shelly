cd825ecf643c26ca3a9b8a1ef2e98b12f6e7033a
Alexander Ryden <alexander.ryden@gmail.com>
2018-12-15 16:01:32 +0100

    Add support for the Shelly1

--- index.js
+++ index.js
@@ -5,7 +5,91 @@ module.exports = homebridge => {
   const Characteristic = homebridge.hap.Characteristic
   const PlatformAccessory = homebridge.platformAccessory
   const Service = homebridge.hap.Service
-  const UUIDGen = homebridge.hap.uuid
+  const uuid = homebridge.hap.uuid
+
+  const createAccessory = (device, name) => {
+    const accessory = new PlatformAccessory(name, uuid.generate(name))
+
+    const infoService = accessory.getService(Service.AccessoryInformation)
+      .setCharacteristic(Characteristic.Manufacturer, 'Shelly')
+      .setCharacteristic(Characteristic.Model, device.type)
+      .setCharacteristic(Characteristic.SerialNumber, device.id)
+
+    if (device.settings && device.settings.fw) {
+      infoService.setCharacteristic(
+        Characteristic.FirmwareRevision,
+        device.settings.fw
+      )
+    }
+
+    if (device.settings && device.settings.hwinfo) {
+      infoService.setCharacteristic(
+        Characteristic.HardwareRevision,
+        device.settings.hwinfo.hw_revision
+      )
+    }
+
+    accessory.context = {
+      type: device.type,
+      id: device.id,
+      host: device.host,
+    }
+
+    accessory.updateReachability(device.online)
+
+    return accessory
+  }
+
+  const createShelly1RelayAccessory = device => {
+    const accessory = createAccessory(
+      device,
+      device.name || `${device.type} ${device.id}`
+    )
+
+    accessory.category = Accessory.Categories.SWITCH
+
+    accessory.addService(
+      new Service.Switch()
+        .setCharacteristic(Characteristic.On, device.relay0)
+    )
+
+    setupShelly1RelayHandlers(device, accessory)
+
+    return accessory
+  }
+
+  const createAccessoriesForDevice = device => {
+    let accessories = []
+
+    if (device.type === 'SHSW-1') {
+      accessories.push(createShelly1RelayAccessory(device))
+    }
+
+    return accessories
+  }
+
+  const setupShelly1RelayHandlers = (device, accessory) => {
+    const onCharacteristic = accessory
+      .getService(Service.Switch)
+      .getCharacteristic(Characteristic.On)
+      .on('set', async (newValue, callback) => {
+        await device.setRelay(0, newValue)
+        callback()
+      })
+
+    device.on('change:relay0', newValue => {
+      onCharacteristic.setValue(newValue)
+    })
+  }
+
+  const setupHandlersForAccessory = (device, accessory) => {
+    device.on('online', () => { accessory.updateReachability(true) })
+    device.on('offline', () => { accessory.updateReachability(false) })
+
+    if (device.type === 'SHSW-1') {
+      setupShelly1RelayHandlers(device, accessory)
+    }
+  }
 
   class ShellyPlatform {
     constructor(log, config, api) {
@@ -18,60 +102,40 @@ module.exports = homebridge => {
     }
 
     discoverDeviceHandler(device) {
-      const accessory = this.createAccessory(device)
-      this.api.registerPlatformAccessories(
-        'homebridge-shelly',
-        'Shelly',
-        [accessory]
-      )
-    }
+      const accessories = createAccessoriesForDevice(device)
 
-    createAccessory(device) {
-      const name = device.name || `${device.type} ${device.id}`
-      const accessory = new PlatformAccessory(
-        name,
-        UUIDGen.generate(name),
-        Accessory.Categories.SWITCH
-      )
-
-      accessory.getService(Service.AccessoryInformation)
-        .setCharacteristic(Characteristic.Manufacturer, 'Shelly')
-        .setCharacteristic(Characteristic.Model, device.type)
-        .setCharacteristic(Characteristic.SerialNumber, device.id)
-        .setCharacteristic(Characteristic.FirmwareRevision, device.settings.fw)
-
-      accessory.addService(
-        new Service.Switch()
-          .setCharacteristic(Characteristic.On, true)
-      )
-
-      accessory.context = {
-        type: device.type,
-        id: device.id,
-        host: device.host,
+      if (accessories.length > 0) {
+        this.api.registerPlatformAccessories(
+          'homebridge-shelly',
+          'Shelly',
+          accessories
+        )
       }
-
-      return accessory
     }
 
     async configureAccessory(accessory) {
       const ctx = accessory.context
-      const device = shellies.createDevice(ctx.type, ctx.id, ctx.host)
+      let device = shellies.getDevice(ctx.type, ctx.id)
 
-      shellies.addDevice(device)
+      if (!device) {
+        device = shellies.createDevice(ctx.type, ctx.id, ctx.host)
+        shellies.addDevice(device)
 
-      try {
-        device.settings = await device.getSettings()
-      } catch (e) {
-        this.log(
-          'Device with ID',
-          device.id,
-          'and address',
-          device.host,
-          'unreachable'
-        )
-        device.online = false
+        try {
+          device.settings = await device.getSettings()
+        } catch (e) {
+          this.log(
+            'Device with ID',
+            device.id,
+            'and address',
+            device.host,
+            'is unreachable'
+          )
+          device.online = false
+        }
       }
+
+      setupHandlersForAccessory(device, accessory)
     }
   }
 
