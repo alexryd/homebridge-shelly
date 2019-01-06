93e5096b749e9d2b120acfb423e087123dcf70f7
Alexander Ryden <alexander.ryden@gmail.com>
2019-01-06 10:38:42 +0100

    Add tests for handleFailedRequest()

---
+++ test/test-error-handlers.js
@@ -0,0 +1,22 @@
+/* eslint-env mocha */
+
+const sinon = require('sinon')
+
+const log = require('./mocks/log')
+
+const { handleFailedRequest } = require('../error-handlers')
+
+describe('handleFailedRequest', function() {
+  it('should log error messages', function() {
+    const logStub = sinon.stub(log, 'log')
+    const device = {
+      type: 'SHSW-1',
+      id: 'ABC123',
+      host: '192.168.1.2',
+    }
+
+    handleFailedRequest(log, device, {})
+
+    logStub.called.should.equal(true)
+  })
+})
