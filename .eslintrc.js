16b288e2d915b919cd3214449372ab5c0ff35a6b
Alexander Ryden <alexander.ryden@gmail.com>
2019-01-06 09:28:13 +0100

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "env": {
+    "node": true
+  },
+
+  "extends": [
+    "eslint:recommended",
+    "standard"
+  ],
+
+  "rules": {
+    "comma-dangle": ["error", "only-multiline"],
+
+    "max-len": ["error", {
+      "code": 80
+    }],
+
+    "space-before-function-paren": ["error", {
+      "anonymous": "never",
+      "named": "never",
+      "asyncArrow": "always"
+    }]
+  }
+}
