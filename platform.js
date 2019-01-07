0b858e4021ca66152d216e6ed9f7f2c7198f1768
Alexander Ryden <alexander.ryden@gmail.com>
2019-01-07 09:14:42 +0100

    No need to set the device to offline

--- platform.js
+++ platform.js
@@ -114,7 +114,6 @@ module.exports = homebridge => {
 
       if (!device) {
         device = shellies.createDevice(ctx.type, ctx.id, ctx.host)
-        device.online = false
         shellies.addDevice(device)
       }
 
