50f49b8a8f9e17d7e91df84a970767a3d7d30bb2
Manuel Faux <mfaux@conf.at>
2023-01-16 00:39:10 +0100

    Expose second button to homebridge.
    Added two new types for Shelly 1L: statelessSwitch (short/double/long)
    and statelessToggle (short on each activation (0->1 or 1->0).

--- accessories/stateless-switches.js
+++ accessories/stateless-switches.js
@@ -2,8 +2,10 @@
 module.exports = homebridge => {
   const BatteryAbility = require('../abilities/battery')(homebridge)
   const ServiceLabelAbility = require('../abilities/service-label')(homebridge)
-  const StatelessProgrammableSwitchAbility =
-    require('../abilities/stateless-programmable-switch')(homebridge)
+  const {
+    StatelessProgrammableSwitchAbility,
+    StatelessProgrammableToggleAbility,
+  } = require('../abilities/stateless-programmable-switch')(homebridge)
   const { ShellyAccessory } = require('./base')(homebridge)
 
   class ShellyButton1StatelessSwitchAccessory extends ShellyAccessory {
@@ -19,19 +21,42 @@ module.exports = homebridge => {
   }
 
   class ShellyInputStatelessSwitchAccessory extends ShellyAccessory {
-    constructor(device, index, config, log, numberOfInputs = 1) {
+    constructor(device, index, config, log, numberOfInputs = 1, numberOfFirstInput = 0) {
       super('statelessSwitch', device, index, config, log)
 
       if (numberOfInputs === 1) {
         this.abilities.push(new StatelessProgrammableSwitchAbility(
-          'inputEvent0',
-          'inputEventCounter0'
+          'inputEvent' + numberOfFirstInput,
+          'inputEventCounter' + numberOfFirstInput
         ))
       } else {
         for (let i = 0; i < numberOfInputs; i++) {
           this.abilities.push(new StatelessProgrammableSwitchAbility(
-            'inputEvent' + i,
-            'inputEventCounter' + i,
+            'inputEvent' + (i + numberOfFirstInput),
+            'inputEventCounter' + (i + numberOfFirstInput),
+            i + 1
+          ))
+        }
+
+        this.abilities.push(new ServiceLabelAbility())
+      }
+    }
+  }
+
+  class ShellyInputStatelessToggleAccessory extends ShellyAccessory {
+    constructor(device, index, config, log, numberOfInputs = 1, numberOfFirstInput = 0) {
+      super('statelessToggle', device, index, config, log)
+
+      if (numberOfInputs === 1) {
+        this.abilities.push(new StatelessProgrammableToggleAbility(
+          'input' + numberOfFirstInput,
+          'inputEventCounter' + numberOfFirstInput
+        ))
+      } else {
+        for (let i = 0; i < numberOfInputs; i++) {
+          this.abilities.push(new StatelessProgrammableToggleAbility(
+            'input' + (i + numberOfFirstInput),
+            'inputEventCounter' + (i + numberOfFirstInput),
             i + 1
           ))
         }
@@ -44,5 +69,6 @@ module.exports = homebridge => {
   return {
     ShellyButton1StatelessSwitchAccessory,
     ShellyInputStatelessSwitchAccessory,
+    ShellyInputStatelessToggleAccessory,
   }
 }
